e79501aeb7e6d1217310a606430f09fc49431cac
Samuel McConkie <samuelmcconkie@gmail.com>
2024-09-16 21:45:06 -0600

    Implemented full functionality for pawn tests. Only fails on comparison, but contains all the correct values

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -6,6 +6,13 @@ public class PawnMovesCalculator extends PieceMovesCalculator {
         createMoves();
     }
 
+    private void promotePawn(ChessPosition newPosition) {
+        myMoves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.BISHOP));
+        myMoves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.KNIGHT));
+        myMoves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.QUEEN));
+        myMoves.add(new ChessMove(myPosition, newPosition, ChessPiece.PieceType.ROOK));
+    }
+
     public void createMoves() {
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
@@ -16,30 +23,60 @@ public class PawnMovesCalculator extends PieceMovesCalculator {
                 ChessPosition newPosition = new ChessPosition(row + 1, col + mod);
                 ChessPiece other = board.getPiece(newPosition);
                 if (other != null && other.getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                    ChessMove myMove = new ChessMove(myPosition, newPosition, null);
-                    myMoves.add(myMove);
+                    if (newPosition.getRow() == 8) {
+                        promotePawn(newPosition);
+                    } else {
+                        ChessMove myMove = new ChessMove(myPosition, newPosition, null);
+                        myMoves.add(myMove);
+                    }
                 }
             }
             ChessPosition newPosition = new ChessPosition(row + 1, col);
             if (board.getPiece(newPosition) == null) {
-                ChessMove myMove = new ChessMove(myPosition, newPosition, null);
-                myMoves.add(myMove);
+                if (newPosition.getRow() == 8) {
+                    promotePawn(newPosition);
+                } else {
+                    ChessMove myMove = new ChessMove(myPosition, newPosition, null);
+                    myMoves.add(myMove);
+                }
+                if (myPosition.getRow() == 2) {
+                    ChessPosition otherPosition = new ChessPosition(row + 2, col);
+                    if (board.getPiece(otherPosition) == null) {
+                        ChessMove newMove = new ChessMove(myPosition, otherPosition, null);
+                        myMoves.add(newMove);
+                    }
+                }
             }
         } else {
             for (int mod : i) {
                 ChessPosition newPosition = new ChessPosition(row - 1, col + mod);
                 ChessPiece other = board.getPiece(newPosition);
                 if (other != null && other.getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                    ChessMove myMove = new ChessMove(myPosition, newPosition, null);
-                    myMoves.add(myMove);
+                    if (newPosition.getRow() == 1) {
+                        promotePawn(newPosition);
+                    } else {
+                        ChessMove myMove = new ChessMove(myPosition, newPosition, null);
+                        myMoves.add(myMove);
+                    }
                 }
             }
             ChessPosition newPosition = new ChessPosition(row - 1, col);
             if (board.getPiece(newPosition) == null) {
-                ChessMove myMove = new ChessMove(myPosition, newPosition, null);
-                myMoves.add(myMove);
-
+                if (newPosition.getRow() == 1) {
+                    promotePawn(newPosition);
+                } else {
+                    ChessMove myMove = new ChessMove(myPosition, newPosition, null);
+                    myMoves.add(myMove);
+                }
+                if (myPosition.getRow() == 7) {
+                    ChessPosition otherPosition = new ChessPosition(row - 2, col);
+                    if (board.getPiece(otherPosition) == null) {
+                        ChessMove newMove = new ChessMove(myPosition, otherPosition, null);
+                        myMoves.add(newMove);
+                    }
+                }
             }
+
         }
     }
 }
