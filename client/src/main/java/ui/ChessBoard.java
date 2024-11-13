05cb74125b16b87eb0c342c6260eaa7a4e5dfe2c
Samuel McConkie <samuelmcconkie@gmail.com>
2024-11-12 23:51:44 -0700

    started implementation of chessboard class

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -1,4 +1,34 @@
 package ui;
 
+import chess.ChessGame;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static chess.ChessGame.TeamColor.*;
+import static ui.EscapeSequences.*;
+
+
 public class ChessBoard {
+
+    public ChessBoard() {
+
+    }
+
+    private void printBoard(ChessGame.TeamColor color) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        out.print(ERASE_SCREEN);
+        drawHeader(out, color);
+        drawBoard(out, color);
+    }
+
+    private void drawHeader(PrintStream out, ChessGame.TeamColor color) {
+        //code to draw the first and last row from the team's desired color
+    }
+
+    private void drawBoard(PrintStream out, ChessGame.TeamColor color) {
+
+    }
 }
+
