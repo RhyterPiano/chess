eedfe2ca8b4d7bd653768c89ee7deefe6ccb6368
Samuel McConkie <samuelmcconkie@gmail.com>
2024-11-14 00:47:15 -0700

    added all options to client class

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,14 +1,17 @@
 package ui;
 
+import requests.*;
 import ui.network.ServerFacade;
 
 import java.util.Arrays;
 
 public class Client {
     ServerFacade serverFacade;
+    private boolean loggedIn;
 
     public Client() {
         serverFacade = new ServerFacade("http://localhost:8080");
+        loggedIn = false;
     }
 
     public void run() {
@@ -20,19 +23,64 @@ public class Client {
             var tokens = input.toLowerCase().split(" ");
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
-            return switch (cmd) {
-                case "help" -> help();
-                case "quit" -> "quit";
-                case "login" -> login();
-                case "register" -> register();
-                default -> help();
-            };
+            if (loggedIn) {
+                return switch (cmd) {
+                    case "help" -> help();
+                    case "logout" -> logout();
+                    case "create" -> creatGame();
+                    case "list" -> listGames();
+                    case "play" -> playGame();
+                    case "observe" -> observeGame();
+                    default -> help();
+                }
+            } else {
+                return switch (cmd) {
+                    case "help" -> help();
+                    case "quit" -> "quit";
+                    case "login" -> login(params);
+                    case "register" -> register(params);
+                    default -> help();
+                };
+            }
+
         } catch (Exception ex) {
             return ex.getMessage();
         }
     }
 
+    private String observeGame() {
+    }
+
+    private String playGame() {
+    }
+
+    private String listGames() {
+    }
+
+    private String creatGame() {
+    }
+
+    private String logout() {
+        try {
+            serverFacade.logout();
+            loggedIn = false;
+            return "Successfully logged out!";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
     public String help() {
+        if(loggedIn) {
+            return """
+                    -help
+                    -logout
+                    -create
+                    -list
+                    -play
+                    -observe
+                    """;
+        }
         return """
                 -help
                 -quit
@@ -42,12 +90,28 @@ public class Client {
     }
 
     public String login(String... params) {
-        return "login";
+       try {
+           LoginRequest request = new LoginRequest(params[0], params[1]);
+           serverFacade.login(request);
+           loggedIn = true;
+           return String.format("Logged in as %s.", request.username());
+       } catch (Exception e) {
+           return String.format("Failed to log in. %s\n", e.getMessage());
+       }
     }
 
     public String register(String... params) {
-        return "register";
+        try {
+            RegisterRequest request = new RegisterRequest(params[0], params[1], params[2]);
+            serverFacade.register(request);
+            loggedIn = true;
+            return String.format("Registered user %s, you are now logged in!", request.username());
+        } catch (Exception e) {
+            return String.format("Falled to register. %s", e.getMessage());
+        }
     }
+
+
 
 
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,38 @@
 package ui;
 
+import static chess.EscapeSequences.*;
+import java.util.Scanner;
+
 public class Repl {
+    private Client client;
+
+    public void main() {
+        run();
+    }
+
+    public Repl() {
+        client = new Client();
+    }
+
+    public void run() {
+        System.out.println("Welcome to Chess! Sign in or register to start!");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+
 }
